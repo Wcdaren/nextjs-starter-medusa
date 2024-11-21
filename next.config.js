89b8e85503184f9ad87bbb5a7f95c18aacd46a56
wcdaren <wcdaren@gmail.com>
2024-11-21 16:49:11 +0800

    fix: add new S3 hostname for Lambda Curry sites

--- next.config.js
+++ next.config.js
@@ -23,6 +23,10 @@ const nextConfig = {
       },
       {
         protocol: "https",
+        hostname: "lambdacurrysites.s3.us-east-1.amazonaws.com",
+      },
+      {
+        protocol: "https",
         hostname: "medusa-server-testing.s3.us-east-1.amazonaws.com",
       },
     ],
